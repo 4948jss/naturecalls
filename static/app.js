f78f548f23b0141ff9e5a1a44ed51d2b38483a5a
4948jss <4948jss@gmail.com>
2023-02-04 18:29:17 +0900

    git push 테스트 시연

--- static/app.js
+++ static/app.js
@@ -3,7 +3,7 @@ const mapOption = {
     center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
     level: 3, // 지도의 확대 레벨
 };
-
+// 테스트용커밋
 //전역변수 설정
 var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
