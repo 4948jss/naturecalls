67fd120fd480cfafa918a548299b3be17d7c177b
4948jss <4948jss@gmail.com>
2023-02-04 11:05:50 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+import certifi
+import csv
+from pymongo import MongoClient
+from flask import Flask, render_template, request, jsonify, send_from_directory
+
+app = Flask(__name__)
+ca = certifi.where()
+
+# 원웅님 mongoDB에 연결하기
+client = MongoClient('mongodb+srv://a8sparta:naturecalls@cluster0.rtdxuho.mongodb.net/?retryWrites=true&w=majority',tlsCAFile=ca)
+db = client.Naturecalls
+collection = db["ToiletInfo"]
