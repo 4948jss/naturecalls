70965e8d381f1ee239e5fb8f06514c2c0943b97e
swjeno <81218141+swjeno@users.noreply.github.com>
2023-02-05 13:56:30 +0900

    메인페이지 자기위치에서 시작 : 함수 분리 후 적용 오버레이 마커 반복문으로 삽입

--- static/app.js
+++ static/app.js
@@ -6,6 +6,80 @@ const mapOption = {
 // 테스트용커밋
 //전역변수 설정
 var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+myLocation();//현위치에서 시작
+
+
+// 지도에 마커를 표시하는 함수입니다
+function displayMarker(data) {
+    //이미지src
+    var imageSrc =
+        "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
+    //이미지 size
+    var imageSize = new kakao.maps.Size(24, 35);
+
+    // 마커 이미지 생성
+    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+
+    //마커 생성
+    var marker = new kakao.maps.Marker({
+        image: markerImage, // 마커 이미지
+        map: map,
+        position: data.latlng
+    });
+
+    //커스텀 오버레이 표시
+    var overlay = new kakao.maps.CustomOverlay({
+        yAnchor: 1,
+        position: marker.getPosition()
+    });
+
+    //content표시
+    var content = document.createElement('div');
+    content.innerHTML = data.content;
+    content.className = 'overlay';
+
+    //닫기버튼
+    var closeBtn = document.createElement('button');
+    closeBtn.innerHTML = '닫기';//버튼내용삽입
+    closeBtn.onclick = function () {
+        overlay.setMap(null);
+    };
+    closeBtn.className = 'close'//버튼 클래스 부여(css사용)
+    content.appendChild(closeBtn);//버튼 붙이기
+
+    //리뷰보기 버튼
+    var reviewRBtn = document.createElement('button');
+    reviewRBtn.innerHTML = '리뷰보기';//버튼내용삽입
+    reviewRBtn.onclick = function () {
+        location.href = '/review/edit/review_edit.html';//이동 경로
+    };
+    reviewRBtn.className = 'review'//버튼 클래스 부여(css사용)
+    content.appendChild(reviewRBtn);//버튼 붙이기
+
+    //리뷰쓰기 버튼
+    var reviewWeBtn = document.createElement('button');
+    reviewWeBtn.innerHTML = '리뷰쓰기';//버튼내용삽입
+    reviewWeBtn.onclick = function () {
+        location.href = '/review/write/review_write.html';//이동 경로
+    };
+    reviewWeBtn.className = 'reviewWeBtn'//버튼 클래스 부여(css사용)
+    content.appendChild(reviewWeBtn);//버튼 붙이기
+
+    //신고하기버튼
+    var reportBtn = document.createElement('button');
+    reportBtn.innerHTML = '신고하기';//버튼내용삽입
+    reportBtn.onclick = function () {
+        location.href = '/report/singofin.html';//이동 경로
+    };
+    reportBtn.className = 'reportBtn'//버튼 클래스 부여(css사용)
+    content.appendChild(reportBtn);//버튼 붙이기
+
+    overlay.setContent(content);
+
+    kakao.maps.event.addListener(marker, 'click', function () {
+        overlay.setMap(map);
+    });
+}
 
 function myLocation() {
     // geolocation API를 사용하여 현재 위치 얻어오기
@@ -23,64 +97,55 @@ function myLocation() {
             map: map, // => 전역변수 값 사용
             position: new kakao.maps.LatLng(lat, lng),
         });
-
-        //화장실 데이터 받아오기
-        // 현재 해당 api는 현재 위치의 좌표값으로 받아오는 형태가 아님
-        // 현재 좌표 기준으로 범위의 리스트를 get할수 있는 api를 찾거나,
-        // 모든 데이터를 저장 및 노출하는 step이 필요 => 어떤기준으로 리스트를 주는지 불명확 => 현재위치 기준의 화장실들이 없는 경우 발생
-        $.ajax({
-            type: "GET",
-            url: "/toiletInfo",
-            data: {},
-            success: function (response) {
-                // api 성공시 로직
-
-                // 화장실 데이터 리스트 생성
-                const data = response['toilets'];
-                let positions = [];
-
-                for (var i in data) {
-                    const position = {};
-                    position["toiletnum"] = data[i]["toilet_num"];
-                    position["roadaddr"] = data[i]["roadAddr"];
-                    position["latlng"] = new kakao.maps.LatLng(
-                        data[i]["y_wgs84"],
-                        data[i]["x_wgs84"]
-                    );
-                    positions.push(position);
-                }
-
-                // 화장실 마커 생성
-                if (positions.length > 0) {
-                    var imageSrc =
-                        "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
-
-                    for (var i = 0; i < positions.length; i++) {
-                        // spread
-                        const {FNAME, latlng} = positions[i];
-
-                        // 마커 이미지 크기
-                        var imageSize = new kakao.maps.Size(24, 35);
-
-                        // 마커 이미지 생성
-                        var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
-
-                        // 마커 생성
-                        const marker = new kakao.maps.Marker({
-                            map: map, // 지도 => 전역변수 값 사용
-                            position: latlng, // 마커 표시 위치
-                            title: FNAME, // 마커 타이틀
-                            image: markerImage, // 마커 이미지
-                        });
-                    }
-                } else {
-                    // 데이터가 없을 경우,
-                    alert("현재위치에 화장실이 없습니다.");
-                    return;
-                }
-            },
-        });
     });
+}
+
+function getToilet() {
+
+    //화장실 데이터 받아오기
+    // 현재 해당 api는 현재 위치의 좌표값으로 받아오는 형태가 아님
+    // 현재 좌표 기준으로 범위의 리스트를 get할수 있는 api를 찾거나,
+    // 모든 데이터를 저장 및 노출하는 step이 필요 => 어떤기준으로 리스트를 주는지 불명확 => 현재위치 기준의 화장실들이 없는 경우 발생
+    $.ajax({
+        type: "GET",
+        url: "/toiletInfo",
+        data: {},
+        success: function (response) {
+            // api 성공시 로직
+
+            // 화장실 데이터 리스트 생성
+            const data = response['toilets'];
+            let positions = [];
+
+            for (i = 0; i < data.length; i++) {
+                const position = {};
+                position["content"] = '<div class="wrap">' +
+                    '    <div class="info">' +
+                    '        <div class="title">' + data[i]["toilet_num"] + " : " + data[i]["toilet_name"] + '</div>' +
+                    '        <div class="body">' +
+                    '            <div class="desc">' +
+                    '                <div class="text">' + "구분 : " + data[i]["toilet_class"] + '</div>' +
+                    '                <div class="text">' + "관리자 : " + data[i]["toilet_manager"] + '</div>' +
+                    '                <div class="text">' + "전화번호 : " + data[i]["toilet_phone"] + '</div>' +
+                    '                <div class="text">' + "CCTV : " + data[i]["toilet_cctv"] + '</div>' +
+                    '                <div class="text">' + "신고벨 : " + data[i]["toilet_bell"] + '</div>' +
+                    '                <div class="text">' + "장애인 화장실 : " + data[i]["toilet_disabled"] + '</div>' +
+                    '                <div class="text">' + "기저귀 교환대 : " + data[i]["toilet_diaper"] + '</div>' +
+                    '            </div>' +
+                    '        </div>' +
+                    '    </div>' +
+                    '</div>';
+                position["latlng"] = new kakao.maps.LatLng(data[i]["y_wgs84"], data[i]["x_wgs84"]);
+                positions.push(position);
+            }
+
+            for (let i = 0; i < positions.length; i++) {
+                var toilet = positions[i];
+                displayMarker(toilet);
+            }
+        },
+    });
+
 }
 
 // toilettest 불러오기
